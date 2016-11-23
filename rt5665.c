0f4f659c1847f0c5cd3d956c1321734d51f4f96a
Oder Chiou <oder_chiou@realtek.com>
2016-11-23 11:58:39 +0800

    Fix the reK function
    Signed-off-by: Oder Chiou <oder_chiou@realtek.com>

--- rt5665.c
+++ rt5665.c
@@ -1013,11 +1013,6 @@ static void rt5665_recalibrate(struct snd_soc_codec *codec)
 	static bool recalibrated = false;
 	unsigned int reg13a, reg1db;
 
-	if (recalibrated == false)
-		recalibrated = true;
-	else
-		snd_soc_update_bits(codec, RT5665_HP_CALIB_CTRL_1, 0x1800, 0);
-
 	reg13a = snd_soc_read(codec, RT5665_CHOP_DAC);
 	reg1db = snd_soc_read(codec, RT5665_HP_LOGIC_CTRL_2);
 
@@ -1080,15 +1075,9 @@ static int rt5665_hp_vol_put(struct snd_kcontrol *kcontrol,
 			RT5665_NG2_EN_MASK, RT5665_NG2_EN);
 	}
 
-	if (rt5665->impedance_gain_map == true) {
+	if (rt5665->impedance_gain_map == true)
 		snd_soc_update_bits(codec, RT5665_BIAS_CUR_CTRL_8, 0x0700,
 			rt5665->impedance_bias << 8);
-
-		if (rt5665->do_rek) {
-			rt5665_recalibrate(codec);
-			rt5665->do_rek = false;
-		}
-	}
 
 	mutex_unlock(&codec->component.card->dapm_mutex);
 
@@ -3026,9 +3015,15 @@ static int rt5665_hp_event(struct snd_soc_dapm_widget *w,
 	struct snd_kcontrol *kcontrol, int event)
 {
 	struct snd_soc_codec *codec = w->codec;
+	struct rt5665_priv *rt5665 = snd_soc_codec_get_drvdata(codec);
 
 	switch (event) {
 	case SND_SOC_DAPM_PRE_PMU:
+		if (rt5665->do_rek) {
+			rt5665_recalibrate(codec);
+			rt5665->do_rek = false;
+		}
+
 		snd_soc_update_bits(codec, RT5665_STO_NG2_CTRL_1,
 			RT5665_NG2_EN_MASK, RT5665_NG2_EN);
 		snd_soc_write(codec, RT5665_HP_LOGIC_CTRL_2, 0x0003);
