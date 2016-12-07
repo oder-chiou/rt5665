9cb829ad19689af86b7d34eddb4299fe2f075481
Oder Chiou <oder_chiou@realtek.com>
2016-12-07 10:58:31 +0800

    Add reK function in hp off
    Signed-off-by: Oder Chiou <oder_chiou@realtek.com>

--- rt5665.c
+++ rt5665.c
@@ -1015,8 +1015,9 @@ static const struct snd_kcontrol_new rt5665_if3_adc_swap_mux =
 
 static void rt5665_recalibrate(struct snd_soc_codec *codec)
 {
-	unsigned int reg06b, reg13a, reg1db;
+	unsigned int reg063, reg06b, reg13a, reg1db;
 
+	reg063 = snd_soc_read(codec, RT5665_PWR_ANLG_1);
 	reg06b = snd_soc_read(codec, RT5665_CLK_DET);
 	reg13a = snd_soc_read(codec, RT5665_CHOP_DAC);
 	reg1db = snd_soc_read(codec, RT5665_HP_LOGIC_CTRL_2);
@@ -1049,6 +1050,9 @@ static void rt5665_recalibrate(struct snd_soc_codec *codec)
 	snd_soc_write(codec, RT5665_CALIB_ADC_CTRL, 0x2005);
 	snd_soc_update_bits(codec, RT5665_CHOP_DAC, 0x3000, reg13a);
 	snd_soc_update_bits(codec, RT5665_CLK_DET, 0x8001, reg06b);
+	snd_soc_update_bits(codec, RT5665_PWR_ANLG_1,
+			RT5665_PWR_VREF1 | RT5665_PWR_FV1 |
+			RT5665_PWR_VREF2 | RT5665_PWR_FV2, reg063);
 }
 
 static int rt5665_hp_vol_put(struct snd_kcontrol *kcontrol,
@@ -2088,6 +2092,23 @@ static int set_dmic_clk(struct snd_soc_dapm_widget *w,
 			RT5665_DMIC_CLK_MASK, idx << RT5665_DMIC_CLK_SFT);
 	}
 	return idx;
+}
+
+static int rt5665_capless_event(struct snd_soc_dapm_widget *w,
+	struct snd_kcontrol *kcontrol, int event)
+{
+	struct snd_soc_codec *codec = w->codec;
+
+	switch (event) {
+	case SND_SOC_DAPM_POST_PMD:
+		rt5665_recalibrate(codec);
+		break;
+
+	default:
+		return 0;
+	}
+
+	return 0;
 }
 
 static int rt5665_charge_pump_event(struct snd_soc_dapm_widget *w,
@@ -3722,7 +3743,7 @@ static const struct snd_soc_dapm_widget rt5665_dapm_widgets[] = {
 	SND_SOC_DAPM_SUPPLY("Pump HP", RT5665_DEPOP_1, RT5665_PWR_PUMP_HP_BIT,
 		0, NULL, 0),
 	SND_SOC_DAPM_SUPPLY("Capless", RT5665_DEPOP_1, RT5665_PWR_CAPLESS_BIT,
-		0, NULL, 0),
+		0, rt5665_capless_event, SND_SOC_DAPM_POST_PMD),
 
 	SND_SOC_DAPM_SUPPLY("Charge Pump", SND_SOC_NOPM, 0, 0,
 		rt5665_charge_pump_event, SND_SOC_DAPM_PRE_PMU |
